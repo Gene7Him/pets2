fd71cd6e6d05ebcfda53ecb1ef412bb74763be65
gene7 <faison.eugene@student.greenriver.edu>
2024-04-18 13:40:10 -0400

    initial set up for pets page

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// This is my controller
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once('vendor/autoload.php');
+
+// Instantiate the F3 Base Class
+$f3 = Base::instance();
+
+// define default route
+$f3->route('GET /', function(){
+    //echo '<h1>Hello Fat-Free!</h1>';
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+// Run Fat-Free
+$f3->run();
+
